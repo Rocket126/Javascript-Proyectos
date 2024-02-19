9967d2370b929cfea7b7544a32270d112c5cceea
IniciandoElcambio <carlosgctec@mail.com>
2024-02-19 01:12:00 -0300

    viendo el debugeador o consola en el navegador para javascript

--- Curso JS Moderno/01-Introduccion/js/script.js
+++ Curso JS Moderno/01-Introduccion/js/script.js
@@ -1,4 +1,4 @@
-const nombre = prompt('Cual es tu nombre');//el prompt almacena lo ingresado en la variable nombre
+const nombre = prompt('Cual es tu nombre');//el prompt toma el valor y almacena lo ingresado en la variable nombre
 
 //`${}` esto sellama template string ya que lleva comillas simples invertidas. Para manipular variables 
 
@@ -6,6 +6,8 @@ const nombre = prompt('Cual es tu nombre');//el prompt almacena lo ingresado en 
 document.querySelector('.contenido').innerHTML =`${nombre} está aprendiendo Javascript`;
 // a la clase .contenido reemplaza su "contenido" en el html con el texto que esta en el template string más su varible
 
+console.log("Hola");
 
+console.error("mensaje de error");
 
 //alert("Hola mundo");
