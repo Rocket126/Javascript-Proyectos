69f9710afc48497d923e2bb1b482e070346b5643
IniciandoElcambio <carlosgctec@mail.com>
2024-02-22 00:36:16 -0300

    declarando variables String primitiva y de tipo objeto

---
+++ Curso JS Moderno/02-Variables/js/03-app.js
@@ -0,0 +1,19 @@
+//Palabra reservada const
+// const a diferencia de var y let, const no puede ser reasignado. El valor es constante.
+//las constantes deben tener si o si un valor asignado o indicara error.
+
+const nombre = "Pedro";
+console.log(nombre);
+
+//nombre = "Javier"; 
+//No se puede asignar el valor "Javier" a la constante "nombre" anteriormente declarada.
+
+// las constantes deben llevar un valor asignado.
+//const apellido;//Ya muestra un error donde solicita el valor de la variable.
+
+const apellido = "Cabezas";//Constante con valor asignado. Debe ser asi siempre.
+console.log(apellido);
+
+//ENTONCES
+//las constantes no se pueden reasignar, ni asignar un valor otra linea despues de haber sido declaradas.
+//Deben tener si o si un valor asignado.
