879964a78056e40e0e2823e636cbe47e1653b137
IniciandoElcambio <carlosgctec@mail.com>
2024-05-03 01:49:00 -0400

    repasando arreglos en javascrip, sub arreglos, indices y diferentes elementos almacenados

---
+++ Curso JS Moderno/08-Arrays/js/01-app.js
@@ -0,0 +1,25 @@
+let numeros = [10];
+
+let meses = new Array("Enero", "Febrero", "Marzo");//otra manera de crear un arreglo como un objeto
+
+console.log(numeros);
+console.log(meses);
+
+//un arreglo que contiene datos de todo tipo
+let deTodo = ["hola", 10, true, "si", null, {nombre: "juan", trabajo: "programador"}, [22,25,36]];
+
+console.log(deTodo);
+//Entonces un arreglo puede inclusive contener otro objeto e inclusive objetos como en el ejemplo anterior
+
+//imprime 
+/* 
+0:"hola"
+1:10
+2:true
+3:"si"
+4:null
+5:{nombre: "juan", trabajo: "programador"}
+6:(3)  [0: 22, 1: 25, 2: 36] Largo 3
+
+largo del array detodo: 7
+*/
