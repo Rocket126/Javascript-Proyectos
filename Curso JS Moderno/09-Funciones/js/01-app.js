3d132b77803c8de3a0fda8bbf3c98c128e2384df
IniciandoElcambio <carlosgctec@mail.com>
2024-05-27 20:53:24 -0400

    Repaso de funciones declarativas y funciones expresivas

---
+++ Curso JS Moderno/09-Funciones/js/01-app.js
@@ -0,0 +1,16 @@
+//declaracion de funcion
+
+function sumar()
+{
+    console.log(2+2);
+}
+//llamada de la funcion
+sumar();
+
+
+//Funcion expresiva
+let sumar2 = function()
+{
+    console.log(2+5);
+}
+sumar2();
