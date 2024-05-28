656631056d19f06fe7bc3d6d0a58749d77a30eb9
IniciandoElcambio <carlosgctec@mail.com>
2024-05-28 01:23:23 -0400

    repasando diferencias entre metodos y funciones de JS

---
+++ Curso JS Moderno/09-Funciones/js/03-app.js
@@ -0,0 +1,7 @@
+//funciones definidas en javascrip como alert(); entre algunas
+
+//alert("hola mundo");//funcion 
+
+//prompt("cual es tu edad");//funcion 
+
+//console.log(parseInt("20"));//funcion parseInt() 
---
+++ Curso JS Moderno/09-Funciones/js/04-app.js
@@ -0,0 +1,6 @@
+let numero1 = 20;
+let numero2 = "230";
+
+console.log( parseInt(numero2));//Esto es una Funcion
+console.log(numero1.toString());//Esto es un Metodo
+
